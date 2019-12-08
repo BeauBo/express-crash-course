41e92af57e2615a9bead1dfbbd5045a0e51acf8e
Bo Huang <bo.huang@paytm.com>
2019-12-07 22:36:34 -0500

    adds logger middleware

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@email.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Bob Williams',
+    email: 'bob@email.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Shannon Jackson',
+    email: 'shannon@email.com',
+    status: 'active'
+  }
+]
+
+module.exports = members
--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const path = require('path')
+const members = require('./Members')
+const logger = require('./middlewares/logger')
+
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
 
+// app.use(logger)
+
+app.get('/api/members', (req, res) => {
+  res.json(members)
+})
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
+
+
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
